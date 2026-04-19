3fb69b0e1950144e0d26d575719424528747e67d
2022BCS0102-prathima-sindhu-varshini <prathima7754@gmail.com>
2026-04-19 18:26:37 +0530

    Update model.py

--- app/model.py
+++ app/model.py
@@ -1,18 +1,22 @@
 import pickle
 
+# Load trained model
 model = pickle.load(open("model.pkl", "rb"))
 
 def predict_ml(features):
-    try:
-        X = [[
-            features["tickets_30"],
-            features["monthly_charges"],
-            features["complaints"]
-        ]]
-        
-        pred = model.predict(X)[0]
-        return "HIGH" if pred == 1 else "LOW"
+    """
+    Takes engineered features and returns churn risk
+    """
 
-    except Exception as e:
-        print("ERROR:", e)
-        return "ERROR"
+    # Ensure correct feature order (same as training)
+    X = [[
+        features["tickets_30"],
+        features["monthly_charges"],
+        features["complaints"]
+    ]]
+
+    # Prediction
+    pred = model.predict(X)[0]
+
+    # Convert to business label
+    return "HIGH" if pred == 1 else "LOW"
